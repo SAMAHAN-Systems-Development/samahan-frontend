81ff45616d7a004ca5af2e6fa5546322e484b738
Bernard Pacanza <hyakumachi@users.noreply.github.com>
2025-10-14 23:18:25 +0800

    feat(#009): added Project Card with placeholder image

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -22,16 +22,27 @@ export default function Home() {
         <div className="bg-blue2 h-4 w-4"></div>
       </div>
       <div className="mt-8 space-y-2">
-          <div className="uppercase font-extrabold">Font families and styles:</div>
+        <div className="uppercase font-extrabold">
+          Font families and styles:
+        </div>
         <div className="font-trapix text-lg">- Trapix 400 Regular</div>
         <div className="font-formular-mono text-lg">- Formular 950 Mono</div>
         <div className="font-formular-black text-lg">- Formular 900 Black</div>
-        <div className="font-formular-medium text-lg">- Formular 500 Medium</div>
+        <div className="font-formular-medium text-lg">
+          - Formular 500 Medium
+        </div>
         <div className="font-formular-bold text-lg">- Formular 700 Bold</div>
-        <div className="font-formular-regular text-lg">- Formular 400 Regular</div>
-        <div className="font-fontspring text-lg">- FONTSPRING DEMO - Breul Grotesk A 300 Regular</div>
+        <div className="font-formular-regular text-lg">
+          - Formular 400 Regular
+        </div>
+        <div className="font-fontspring text-lg">
+          - FONTSPRING DEMO - Breul Grotesk A 300 Regular
+        </div>
       </div>
-      <div className="mt-8 font-extrabold uppercase">Test your components below</div>
+      <div className="mt-8 font-extrabold uppercase">
+        Test your components below
+      </div>
+
     </div>
   );
 }
