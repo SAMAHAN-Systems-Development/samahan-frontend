77f32b6b647dd664dd6535ad0ac43277b8c276d7
Ralph Paolo <96500580+ralphpdulce@users.noreply.github.com>
2025-11-09 20:33:34 +0800

    feat: Document Entry Component
    - Created responsive document entry component under src/compnents/sections/document-entry.tsx
    - Installed hugeicons library for gavel icon for "Act Number"
    
    FORMAT FOR COMPONENT PROPS
    - documentName: <string> Name for the legislative document
    - description: <string> Describes more of the legislative document
    - office: <string> Office under which the legal document belongs
    - actNumber: <number> Number of the legislative document {number}
    - year: <number> Year in which the legislative document was passed {number}
    - date: <date>, format: {Month} {Day}, Year. When adding into, format like {new Date("YYYY-MM-DD")}
    - href = <string> Link to file goes here

--- src/components/pages/gazette-page.tsx
+++ src/components/pages/gazette-page.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import ConstitutionFile from '../ConstitutionFile'
+import DocumentEntry from '@/components/sections/document-entry'
 
 function GazettePage() {
   return (
     <div className='flex flex-col justify-center items-center'>
         <ConstitutionFile />
         <div className='h-10'></div>
+        <DocumentEntry
+          documentName='SBN-2817: SAMAHAN POLITICAL AFFAIRS AND ENGAGEMENTS DEPARTMENT'
+          description='An Act Establishing the SAMAHAN Political Affairs and Engagements Department'
+          office='Office of the Legislative Secretary'
+          actNumber={1}
+          year={2024}
+          date={new Date("2024-10-08")}
+          href={"/"}
+        />
+        <DocumentEntry
+          documentName='SBN-2817: SAMAHAN POLITICAL AFFAIRS AND ENGAGEMENTS DEPARTMENT'
+          description='An Act Establishing the SAMAHAN Political Affairs and Engagements Department'
+          office='Office of the Legislative Secretary'
+          actNumber={1}
+          year={2024}
+          date={new Date("2024-10-08")}
+          href={"/"}
+        />
+        <DocumentEntry
+          documentName='SBN-2817: SAMAHAN POLITICAL AFFAIRS AND ENGAGEMENTS DEPARTMENT'
+          description='An Act Establishing the SAMAHAN Political Affairs and Engagements Department'
+          office='Office of the Legislative Secretary'
+          actNumber={1}
+          year={2024}
+          date={new Date("2024-10-08")}
+          href={"/"}
+        />
     </div>
   )
 }
---
+++ src/components/sections/document-entry.tsx
@@ -0,0 +1,61 @@
+//Offices
+//Offices
+import { GoLaw } from "react-icons/go";
+import { CourtLawIcon } from "hugeicons-react";
+import { FaRegCalendar } from "react-icons/fa";
+import Link from "next/link";
+
+interface DocumentEntryProps {
+    documentName: string;
+    description?: string;
+    office?: string;
+    actNumber?: number;
+    year?: number;
+    date?: Date;
+    href: string;
+}
+
+export default function DocumentEntry({ documentName, description, office, actNumber, date, href }: DocumentEntryProps){
+    return (
+        <Link href={href} className="group container flex flex-col py-10 px-4 hover:bg-mainblue transition-colors duration-200  border-b-2 border-mainblue hover:border-mainblue">
+            {/* Document Name */}
+            <div className="document-name font-formular-bold text-lg text-mainblue font-black group-hover:text-white">
+                {documentName}
+            </div>
+            {/* Description */}
+            <div className="description py-3 font-formular-regular text-lg text-mainblue group-hover:text-white">
+                {description}
+            </div>
+            {/* Office, Act Number, Date */}
+            <div className="flex flex-col md:flex-row py-3 space-y-4 md:space-y-0 md:space-x-8 items-start">
+                {/* Office */}
+                {office && (
+                    <div className="office flex items-center space-x-2">
+                        <GoLaw className="text-mainblue group-hover:text-white" size={16}/>
+                        <span className="font-formular-mono text-xs text-mainblue group-hover:text-white">{office}</span>
+                    </div>
+                )}
+                {/* Act Number */}
+                {actNumber && (
+                    <div className="act-no flex items-center space-x-2">
+                        <CourtLawIcon className="text-mainblue group-hover:text-white" size={16} strokeWidth={1.5} />
+                        <span className="font-formular-mono text-mainblue text-xs group-hover:text-white">SAMAHAN Act No. {actNumber} of 2024</span>
+                    </div>
+                )}
+                {/* Date */}
+                {date && (
+                    <div className="date flex items-center space-x-2">
+                        <FaRegCalendar className="text-mainblue group-hover:text-white" size={16} />
+                        <span className="font-formular-mono text-xs text-mainblue group-hover:text-white">
+                            {date.toLocaleDateString('en-US', {
+                                month: 'long',
+                                day: 'numeric',
+                                year: 'numeric',
+                            })}
+                        </span>
+                    </div>
+                )}
+            </div>
+        </Link>
+    )
+}
