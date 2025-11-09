625807c05624a30d0b863870ab44753e44e8abad
Ralph Paolo <96500580+ralphpdulce@users.noreply.github.com>
2025-11-09 20:42:18 +0800

    Remove DocumentEntry components
    Removed multiple DocumentEntry components for the same document, which were used for testing purposes.
    
    Signed-off-by: Ralph Paolo <96500580+ralphpdulce@users.noreply.github.com>

--- src/components/pages/gazette-page.tsx
+++ src/components/pages/gazette-page.tsx
@@ -7,33 +7,6 @@ function GazettePage() {
     <div className='flex flex-col justify-center items-center'>
         <ConstitutionFile />
         <div className='h-10'></div>
-        <DocumentEntry
-          documentName='SBN-2817: SAMAHAN POLITICAL AFFAIRS AND ENGAGEMENTS DEPARTMENT'
-          description='An Act Establishing the SAMAHAN Political Affairs and Engagements Department'
-          office='Office of the Legislative Secretary'
-          actNumber={1}
-          year={2024}
-          date={new Date("2024-10-08")}
-          href={"/"}
-        />
-        <DocumentEntry
-          documentName='SBN-2817: SAMAHAN POLITICAL AFFAIRS AND ENGAGEMENTS DEPARTMENT'
-          description='An Act Establishing the SAMAHAN Political Affairs and Engagements Department'
-          office='Office of the Legislative Secretary'
-          actNumber={1}
-          year={2024}
-          date={new Date("2024-10-08")}
-          href={"/"}
-        />
-        <DocumentEntry
-          documentName='SBN-2817: SAMAHAN POLITICAL AFFAIRS AND ENGAGEMENTS DEPARTMENT'
-          description='An Act Establishing the SAMAHAN Political Affairs and Engagements Department'
-          office='Office of the Legislative Secretary'
-          actNumber={1}
-          year={2024}
-          date={new Date("2024-10-08")}
-          href={"/"}
-        />
     </div>
   )
 }
