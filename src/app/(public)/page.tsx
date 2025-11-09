5dbca60b8037981650f01e0ceca1e269604e1701
edgee20 <ejfusingan0@gmail.com>
2025-11-09 17:26:29 +0800

    feat: created ICOMPP equipment tab section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,7 @@
-import { Metadata } from 'next';
-import { advocasixData } from '@/data/advocasix-card-data';
-import { sdgData } from '@/data/sdg-card-data';
-import HomePageClient from '@/components/pages/home-page';
-
+import { Metadata } from "next";
+import { advocasixData } from "@/data/advocasix-card-data";
+import { sdgData } from "@/data/sdg-card-data";
+import HomePageClient from "@/components/pages/home-page";
 export const metadata: Metadata = {
   title: "SAMAHAN Website 2025-2026",
   description: "Frontend for Samahan Website",
@@ -15,5 +14,3 @@ export default function Home() {
     </>
   );
 }
-
-
