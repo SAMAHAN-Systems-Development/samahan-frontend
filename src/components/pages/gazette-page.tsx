46a3e66802d8475e3ca185a4658bb68531a3b2cb
John Lampa <jklampa31@gmail.com>
2025-11-13 22:34:57 +0800

    fix: removed unused import

--- src/components/pages/gazette-page.tsx
+++ src/components/pages/gazette-page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ConstitutionFile from '../ConstitutionFile'
-import DocumentEntry from '@/components/sections/document-entry'
 
 function GazettePage() {
   return (
