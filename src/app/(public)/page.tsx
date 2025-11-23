93d6bd152188d4c225914cc0e4cd9be001ef7c4d
kyloren01 <kjocalong@addu.edu.ph>
2025-11-23 17:15:29 +0800

    Created the request document tab section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   title: "SAMAHAN Website 2025-2026",
   description: "Frontend for Samahan Website",
 };
-
+import DocumentRequestTabSection from "@/components/sections/request-document-tab-section";
 export default function Home() {
   return (
     <>
