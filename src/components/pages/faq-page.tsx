be3a064ee2b618823c2be5beee0f22059230a5f1
John Lampa <jklampa31@gmail.com>
2025-11-23 18:12:42 +0800

    feat: added faq page

---
+++ src/components/pages/faq-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FAQCategorySection from '../sections/faq-category-section'
+
+function FAQPage() {
+  return (
+    <div>
+        <FAQCategorySection />
+    </div>
+  )
+}
+
+export default FAQPage
