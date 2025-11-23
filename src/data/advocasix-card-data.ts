671a4bf9fccc0d57e2f264e959f8993c46a5343c
kyloren01 <kjocalong@addu.edu.ph>
2025-11-23 18:59:14 +0800

    Fixed the missing advocasix card picture

--- src/data/advocasix-card-data.ts
+++ src/data/advocasix-card-data.ts
@@ -10,32 +10,56 @@ export interface AdvocasixCardData {
 export const advocasixData: AdvocasixCardData[] = [
   {
     cardTitle: "Climate Action",
-    image: { imgSource: "/images/placeholder.png", imgAlt: "placeholder" },
-    content: "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:"
+    image: {
+      imgSource: "/images/advocasix-images/climateaction.jpg",
+      imgAlt: "placeholder",
+    },
+    content:
+      "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:",
   },
   {
     cardTitle: "good governance",
-    image: { imgSource: "/images/placeholder.png", imgAlt: "placeholder" },
-    content: "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:"
+    image: {
+      imgSource: "/images/advocasix-images/goodgovernance.jpg",
+      imgAlt: "placeholder",
+    },
+    content:
+      "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:",
   },
   {
     cardTitle: "holistic health",
-    image: { imgSource: "/images/placeholder.png", imgAlt: "placeholder" },
-    content: "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:"
+    image: {
+      imgSource: "/images/advocasix-images/holistichealth.jpg",
+      imgAlt: "placeholder",
+    },
+    content:
+      "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:",
   },
   {
     cardTitle: "quality education",
-    image: { imgSource: "/images/placeholder.png", imgAlt: "placeholder" },
-    content: "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:"
+    image: {
+      imgSource: "/images/advocasix-images/qualityeducation.jpg",
+      imgAlt: "placeholder",
+    },
+    content:
+      "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:",
   },
   {
     cardTitle: "youth empowerment",
-    image: { imgSource: "/images/placeholder.png", imgAlt: "placeholder" },
-    content: "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:"
+    image: {
+      imgSource: "/images/advocasix-images/youthempowerment.jpg",
+      imgAlt: "placeholder",
+    },
+    content:
+      "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:",
   },
   {
     cardTitle: "sogieSC",
-    image: { imgSource: "/images/placeholder.png", imgAlt: "placeholder" },
-    content: "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:"
+    image: {
+      imgSource: "/images/advocasix-images/soigesc.jpg",
+      imgAlt: "placeholder",
+    },
+    content:
+      "Guided by its principles as a Jesuit, Filipino, and Mindanaoan autonomous student government, the Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao set the goal of championing the identified AdvocaSIX for the incoming 6 years that started on 2022 and aims to cultivate until 2028. With that, SAMAHAN shall hereby champion the following advocacies for the term of 2024-2025:",
   },
 ];
