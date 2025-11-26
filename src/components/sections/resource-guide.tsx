7846882fc665c67ad4773848f04d44a27ca2f668
John Lampa <jklampa31@gmail.com>
2025-11-26 23:16:33 +0800

    fix: added tab section to academix page. fixed spacing and background design

--- src/components/sections/resource-guide.tsx
+++ src/components/sections/resource-guide.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ResourceGuide() {
   return (
-    <section className="flex w-full flex-col items-center overflow-x-clip px-6 py-12 md:px-8 md:py-16 lg:px-12">
+    <section className="flex w-full flex-col items-center overflow-x-clip px-6 pt-3 pb-12 md:px-8 md:pb-16 lg:pb-12">
       <div className="w-full max-w-6xl">
         <h1 className="font-formular-black text-center text-xl text-mainblue md:text-3xl lg:text-4xl xl:text-6xl">
           STUDENT RESOURCE GUIDE
@@ -34,7 +34,7 @@ export default function ResourceGuide() {
       </div>
 
       <div className="relative mt-12 md:mt-16">
-        <div className="pointer-events-none absolute left-1/2 top-1/2 z-0 h-[900px] w-[200vw] -translate-x-1/2 -translate-y-1/2 md:h-[700px] md:w-[1400px] lg:h-[800px] lg:w-[1600px]">
+        <div className="md:hidden pointer-events-none absolute left-1/2 top-1/2 z-0 h-[900px] w-[200vw] -translate-x-1/2 -translate-y-1/2 md:h-[700px] md:w-[1400px] lg:h-[800px] lg:w-[1600px] scale-125 sm:scale-100 -rotate-45 xs:rotate-0">
           <Image
             src="/images/tape-design/tape-1.png"
             alt="Decorative tape design"
@@ -43,8 +43,8 @@ export default function ResourceGuide() {
           />
         </div>
 
-        <div className="relative z-20 mx-auto flex flex-col items-center gap-4 md:flex-row md:justify-center md:gap-6 lg:gap-8">
-          <div className="relative h-42 w-72 overflow-hidden rounded-2xl shadow-lg md:h-96 md:w-96">
+        <div className="relative z-20 mx-auto flex flex-col items-center gap-4 lg:flex-row lg:justify-center md:gap-6 lg:gap-6 xl:gap-8">
+          <div className="relative h-42 w-72 overflow-hidden rounded-2xl shadow-lg md:h-64 md:w-96 lg:h-96 lg:w-96">
             <Image
               src="/images/student-resource/student-resource-1.jpg"
               alt="Student Resource Guide 1"
@@ -54,7 +54,7 @@ export default function ResourceGuide() {
           </div>
 
           <div className="flex flex-col gap-4 md:gap-6">
-            <div className="relative h-42 w-72 overflow-hidden rounded-2xl shadow-lg md:h-80 md:w-78">
+            <div className="relative h-42 w-72 overflow-hidden rounded-2xl shadow-lg md:h-64 md:w-96 lg:h-80 lg:w-78">
               <Image
                 src="/images/student-resource/student-resource-2.png"
                 alt="Student Resource Guide 2"
@@ -63,7 +63,7 @@ export default function ResourceGuide() {
               />
             </div>
 
-            <div className="relative h-42 w-72 overflow-hidden rounded-2xl shadow-lg md:h-64 md:w-64">
+            <div className="relative h-42 w-72 overflow-hidden rounded-2xl shadow-lg md:h-64 md:w-96 lg:h-64 lg:w-64">
               <Image
                 src="/images/student-resource/student-resource-3.jpg"
                 alt="Student Resource Guide 3"
