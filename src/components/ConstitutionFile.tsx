011a37ebc899a5ef23cd5fea000b9f954903e386
John Lampa <jklampa31@gmail.com>
2025-11-27 20:24:25 +0800

    fix: replaced the consti file

--- src/components/ConstitutionFile.tsx
+++ src/components/ConstitutionFile.tsx
@@ -2,7 +2,7 @@
 
 export default function ConstitutionFile(){
     return (
-        <div className="container border-2 border-mainblue rounded-lg flex flex-col justify-center align-center w-11/12 md:w-3/4 py-10 px-5">
+        <div className="border-2 border-mainblue rounded-lg flex flex-col justify-center align-center w-full max-w-[977px] py-10 px-5">
             <div className="title font-trapix text-xl sm:text-3xl md:text-4xl text-center text-mainblue">
                 AMENDED 2020 SAMAHAN CONSTITUTION
             </div>
@@ -10,7 +10,7 @@ export default function ConstitutionFile(){
                 The foundational legal document outlining the structure and function of the student government.
             </div>
             <div className="button-container flex justify-center py-4">
-                <button onClick={() => window.open("/constitutionfile_dummy/dummy.jpg", "_blank")}
+                <button onClick={() => window.open("/documents/gazette-documents/AMENDED-2020-SAMAHAN-CONSTITUTION.pdf", "_blank")}
                     className="bg-mainblue hover:bg-blue2 cursor-pointer text-white text-sm sm:text-md font-formular-regular py-2 px-4 rounded-lg w-7/8">
                     View PDF
                 </button>
