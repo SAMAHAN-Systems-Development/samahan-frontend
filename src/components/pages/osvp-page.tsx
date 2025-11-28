ae431bc1dd6e2273538da4b9f30aa56f94a3a1b1
John Lampa <jklampa31@gmail.com>
2025-11-29 00:54:12 +0800

    feat: added profile cards for osp, osvp, osg, ost, scb, devs

--- src/components/pages/osvp-page.tsx
+++ src/components/pages/osvp-page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import HeroBanner from '../ui/hero-banner'
 import OtherOffices from '../sections/other-offices'
+import ProfileGroup from '../sections/profile-group'
+import ProfileCard from '../ui/profile-card'
+import ProjectCarouselSection from '../sections/project-carousel-section'
 
 function OSVPPage() {
   return (
@@ -11,7 +14,169 @@ function OSVPPage() {
         header1="OFFICE OF THE"
         header2="VICE PRESIDENT"
       />
+
+      <ProfileGroup 
+        groupTitle='OFFICE OF THE SAMAHAN VICE PRESIDENT' 
+        description='Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam ibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.'
+      >
+        <div className="grid grid-cols-2 lg:grid-cols-6 gap-x-4 gap-y-12 lg:gap-x-10 justify-items-center">
+          
+          <div className="col-span-2 lg:col-start-3 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/ken-hinojales.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+          <div className="col-span-1 lg:col-start-2 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/kate-gingatan.png',
+                imageAlt: 'Profile Picture'
+              }}
+              variant="taller"
+            />
+          </div>
+
+          <div className="col-span-1 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/robs-briones.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+        </div>
+      </ProfileGroup>
+
+      <ProfileGroup 
+        groupTitle='OFFICE OF THE LEGISLATIVE SECRETARY' 
+        description='Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam ibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.'
+      >
+        <div className="grid grid-cols-2 lg:grid-cols-6 gap-x-4 gap-y-12 lg:gap-x-10 justify-items-center">
+
+          <div className="col-span-1 lg:col-start-1 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/juelianna-muhali.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+          <div className="col-span-1 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/monica-rama.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+          <div className="col-span-2 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/russell-fajardo.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+        </div>
+      </ProfileGroup>
+
+      <ProfileGroup 
+        groupTitle='DEPARTMENT OF ACADEMIC AFFAIRS' 
+        description='The SAMAHAN Department of Academic Affairs (DAA) serves as the academic arm of the SAMAHAN, under the Office of the SAMAHAN Vice President. 
+At its core, the department is committed to promoting the academic welfare of the student body. It strives to provide support through various initiatives that respond to students’ academic needs. The DAA also plays a role in examining and reflecting on academic policies, contributing student-centered insights that help improve the learning environment.'
+      >
+        <div className="grid grid-cols-2 lg:grid-cols-6 gap-x-4 gap-y-12 lg:gap-x-10 justify-items-center">
+
+          <div className="col-span-1 lg:col-start-2 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/hyacinth-duldulao.png',
+                imageAlt: 'Profile Picture'
+              }}
+              variant="taller"
+            />
+          </div>
+
+          <div className="col-span-1 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/emjay-serna.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+        </div>
+      </ProfileGroup>
+
+      <ProfileGroup 
+        groupTitle='DEPARTMENT OF DISASTER RISK REDUCTION AND MANAGEMENT' 
+        description='The SAMAHAN Department of Disaster Risk Reduction and Management (DDRRM) is the helping arm of the Ateneo de Davao University by providing its services to its constituents. The organization also aims to develop and implement effective disaster response plans and protocols to ensure the safety of everyone in the university in the event of a disaster. Additionally, the department aims to establish partnerships and collaborations with local government units and other organizations to enhance the university community&apos;s disaster risk reduction and management efforts.'
+      >
+        <div className="grid grid-cols-2 lg:grid-cols-6 gap-x-4 gap-y-12 lg:gap-x-10 justify-items-center">
+
+          <div className="col-span-1 lg:col-start-2 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/neomie-jereza.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+          <div className="col-span-1 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/joshua-chua.png',
+                imageAlt: 'Profile Picture'
+              }}
+              variant="taller"
+            />
+          </div>
+
+        </div>
+      </ProfileGroup>
+
+      <ProfileGroup 
+        groupTitle='SAMAHAN POLITICAL AFFAIRS AND ENGAGEMENTS DEPARTMENT' 
+        description='We, the members of the SAMAHAN Political Affairs and Engagements Department of the Samahan ng mga Mag-Aaral ng Pamantasang Ateneo de Davao, recognizing the crucial role of political engagement and leadership in shaping the future of our university and society, hereby establish this department to promote informed and active participation among students.'
+      >
+        <div className="grid grid-cols-2 lg:grid-cols-6 gap-x-4 gap-y-12 lg:gap-x-10 justify-items-center">
+
+          <div className="col-span-1 lg:col-start-2 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/luis-torres.png',
+                imageAlt: 'Profile Picture'
+              }}
+            />
+          </div>
+
+          <div className="col-span-1 lg:col-span-2">
+            <ProfileCard 
+              image={{
+                imageUrl: '/images/osvp/daniella-cadavez.png',
+                imageAlt: 'Profile Picture'
+              }}
+              variant="taller"
+            />
+          </div>
+
+        </div>
+      </ProfileGroup>
+
+      <ProjectCarouselSection />
+      <div className='h-20'></div>
       <OtherOffices office="osvp" />
+      <div className='h-20'></div>
     </div>
   )
 }
