a3a1e6f26bf5813e65795c27128650aad2cbb68c
John Lampa <jklampa31@gmail.com>
2025-11-30 00:51:21 +0800

    fix: replaced png with svg bg

--- src/components/sections/project-carousel-section.tsx
+++ src/components/sections/project-carousel-section.tsx
@@ -4,16 +4,16 @@ import { presidentProjects } from "@/data/projects-carousel-data";
 
 export default function ProjectCarouselSection() {
   return (
-    <section className="relative w-full overflow-hidden py-12 md:py-16 lg:py-20 2xl:h-[1100px] flex flex-col justify-center">
+    <section className="relative w-full overflow-hidden py-12 md:py-16 lg:py-20 xl:py-22 flex flex-col justify-center">
       
       <div className="absolute inset-0 pointer-events-none">
-        <div className="absolute left-1/2 top-1/2 xl:top-5/9 h-[1000px] w-[260vw] -translate-x-1/2 -translate-y-1/2 sm:h-[900px] sm:w-[220vw] md:h-[650px] md:w-[1600px] lg:h-[750px] lg:w-[2000px] xl:h-[820px] xl:w-[2200px] 2xl:h-[1100px] 2xl:w-[180vw]">
+        <div className="absolute left-1/2 top-1/2 xl:top-5/9 h-[1000px] w-[260vw] -translate-x-1/2 -translate-y-1/2 sm:h-[900px] sm:w-[220vw] md:h-[650px] md:w-[1600px] lg:h-[750px] lg:w-[2000px] xl:h-[900px] xl:w-[2200px]">
           <Image
-            src="/images/tape-design/tape-3.png"
-            alt="Decorative tape design"
-            fill
-            className="object-contain rotate-0"
-            priority
+          src="/images/tape-design/tape-2.svg"
+          alt="Decorative tape design"
+          fill
+          className="object-contain rotate-0"
+          priority
           />
         </div>
       </div>
