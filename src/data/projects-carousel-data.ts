7433d677c02bc7d905e74a32c29941e97ee25d2e
kyloren01 <kjocalong@addu.edu.ph>
2026-01-07 12:23:09 +0800

    Fix: Organized the samahan offices projects

--- src/data/projects-carousel-data.ts
+++ src/data/projects-carousel-data.ts
@@ -22,6 +22,63 @@ export const presidentProjects: Project[] = [
     imageUrl: "/images/samahan-projects/samahan-samaSync.jpg",
     url: "/projects/president-project",
   },
+  {
+    title: "Uniform Assistance & Donation | In partnership with UCEAC",
+    description:
+      "The SAMAHAN, in partnership with UCEAC, launched a collection drive for reusable AdDU uniforms donated by alumni and students. This initiative aims to provide uniform support to scholars and students from low-income families by offering gently used uniforms free of charge. The program promotes student welfare and sustainability while helping reduce financial barriers within the Ateneo community.",
+    imageUrl: "/images/samahan-projects/samahan-osp-uniformAssistance.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "University Locker Rental",
+    description:
+      "The Physical Plant Office (PPO) opened locker reservations for all AdDU students on a first-come, first-served basis. Guidelines and the reservation link were released through official channels, with lockers designated for use at the 3F Finster Hall to provide students with secure on-campus storage.",
+    imageUrl: "/images/samahan-projects/samahan-logo.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "UJam in Faith: The Ateneo Praise Festival 2025",
+    description:
+      "The SAMAHAN launched the first-ever Jam in Faith: The Ateneo Praise Fest, a university-wide worship gathering held on August 14, 2025 at the 4F Martin Hall. The event brought together students, musicians, and campus ministries in a night of music and shared prayer, highlighting the community’s collective movement toward deeper faith, unity, and purpose as part of the university’s ongoing commitment to spiritual formation.",
+    imageUrl: "/images/samahan-projects/samahan-osp-jamInFaith.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "Magis on the Move in Sta. Maria",
+    description:
+      "Magis on the Move held on August 21, 2025, in Sta. Maria, brought together young leaders to engage in SDG-aligned, community-focused initiatives. The program, supported by SAMAHAN iGov, provided participants with tools, guidance, and collaborative spaces to strengthen their leadership skills and foster meaningful local impact.",
+    imageUrl: "/images/samahan-projects/samahan-osp-staMaria.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "SAMAHAN Volo Service ",
+    description:
+      "The SAMAHAN Volo Service Program, launched in partnership with the Ateneo de Davao Scholars Society and supported by OSP and OST’s Scholars Care, provides undergraduate scholars with meaningful opportunities to complete their required service hours. The program encourages a culture of volunteerism within the university, with volunteer slots released on a first-come, first-served basis and verified through certification to be attached to the SSTR at the end of the semester.",
+    imageUrl: "/images/samahan-projects/samahan-osp-volo.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "SAMAHAN iGov: The Ateneo Linkages Hub ",
+    description:
+      "SAMAHAN iGov: The Ateneo Linkages Hub serves as the University’s platform for fostering strategic partnerships, collaborations, and institutional linkages beyond Ateneo de Davao University. Through this initiative, students engage with networks that strengthen advocacies, expand leadership opportunities, and drive meaningful social impact, connecting the Ateneo community with programs and initiatives that inspire service and civic engagement.",
+    imageUrl: "/images/samahan-projects/samahan-osp-linkageHub.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "Approval of Athletes’ Civilian Entry Pass",
+    description:
+      "As part of SAMAHAN’s 4YOU Agenda, the Athletes’ Civilian Entry Pass has been approved by the Office of Student Affairs, allowing student-athletes to enter Ateneo de Davao University in their athletic attire under regulated conditions. Endorsed by the University Athletics Office and the College Athletics Council, this initiative supports student-athletes in balancing their training and academic responsibilities while upholding the University’s dress code and decorum. It reflects SAMAHAN’s commitment to recognizing their dedication, fostering leadership, and promoting adaptability and discernment.",
+    imageUrl: "/images/samahan-projects/samahan-osp-athletePass.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title:
+      "Kick-Off 2025: All the Way Up The DACS Honorary Athletes and Performers Send-Off",
+    description:
+      "The Pre-DACS Honorary Blue Knight’s Send-Off (#KickOff2025) celebrated Ateneo athletes as they prepared to represent the University in the Davao Association of Catholic Schools (DACS) competitions. Kick-Off 2025: All the Way Up celebrates the dedication, resilience, and spirit of Ateneo athletes and performers as they prepare for DACS 2025. Honoring the discipline behind every practice, performance, and victory, this event recognizes how their hard work uplifts not just themselves but the entire Ateneo community. It is a moment for the University to stand together in support, cheering on those who carry Ateneo’s pride and inspiring everyone to rise—All the Way Up—toward shared excellence.",
+    imageUrl: "/images/samahan-projects/samahan-osp-dacs.jpg",
+    url: "/projects/president-project",
+  },
 ];
 export const vicepresidentProjects: Project[] = [
   {
@@ -43,6 +100,27 @@ export const vicepresidentProjects: Project[] = [
     description:
       "Knights at the Round Table 4 (#KATRD4) was held at Finster Auditorium, featuring a panel of experts discussing juvenile accountability and the legal and social responsibilities of youth. The event provided students with insights into youth justice, encouraging critical engagement with laws affecting minors and fostering awareness of civic and legal responsibilities.",
     imageUrl: "/images/samahan-projects/samahan-panagutanNgBataSaBatas.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "ANG PANUKALA: The 5th SAMAHAN Student Assembly Capacity Building",
+    description:
+      "Ang Panukala is a two-day capacity-building workshop designed to strengthen the skills of Ateneo student legislators, equipping them with the knowledge, confidence, and purpose needed to craft responsive policies and actively contribute to student governance.",
+    imageUrl: "/images/samahan-projects/samahan-osvp-angPanukala.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "Magis Through Legis: Legislative Workshop",
+    description:
+      "The Magis Through Legis workshop, conducted by the Legislative Support and Monitoring Bureau (LSMB), provided student leaders with training in legislative writing, parliamentary procedure, and participatory policymaking. Through interactive sessions, simulations, and collaborative exercises, participants enhanced their skills in drafting student-centered policies and navigating deliberative processes, strengthening the capacity and integrity of the Student Assembly to respond effectively to the evolving needs of the Ateneo de Davao University community.",
+    imageUrl: "/images/samahan-projects/samahan-logo.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "SAMAHAN Multisectoral Council Launch",
+    description:
+      "The SAMAHAN Multisectoral Council, the sectoral arm of the student government, was launched to foster inclusive and participatory governance at Ateneo de Davao University. Serving as a consultative and collaborative platform, the council engages student leaders and university administrators, amplifies sectoral voices, and ensures equitable representation for all sectors—including student organizations, minority groups, and cause-based movements—strengthening democratic dialogue and social responsibility across the campus community.",
+    imageUrl: "/images/samahan-projects/samahan-osvp-multicoursal.jpg",
     url: "/projects/president-project",
   },
 ];
@@ -91,6 +169,13 @@ export const treasurerProjects: Project[] = [
     description:
       "Magis Gala 2025 was held on July 21–23, marking the official kickoff of the academic year through a series of community-building activities and celebrations. The event welcomed students into the new school year with the theme of movement and discovery, emphasizing growth, engagement, and the Ateneo spirit of MAGIS as the community embarks on a new year of shared experiences.",
     imageUrl: "/images/samahan-projects/samahan-magisGala.jpg",
+    url: "/projects/president-project",
+  },
+  {
+    title: "The Ateneo All-Access: Free Printing Service",
+    description:
+      "The Ateneo All-Access: Free Printing Service, initiated by SAMAHAN, provides students with a convenient and accessible way to print academic requirements at no cost. This service aims to support the Ateneo community in managing their studies efficiently and easing academic workload.",
+    imageUrl: "/images/samahan-projects/samahan-ost-printing.jpg",
     url: "/projects/president-project",
   },
 ];
